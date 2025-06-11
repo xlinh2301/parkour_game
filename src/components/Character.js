edd413853883e22447a80e1739a5bbcb0a447e9c
Your Name <nxlinh2301@gmail.com>
2025-06-11 16:56:10 +0700

    base setup

--- src/components/Character.js
+++ src/components/Character.js
@@ -4,7 +4,7 @@ export function loadCharacter(scene) {
   const loader = new GLTFLoader();
   loader.load('models/character/luoli_run.glb', (gltf) => {
     const character = gltf.scene;
-    character.scale.set(0.02, 0.02, 0.02);
+    character.scale.set(0.002, 0.002, 0.002);
     character.position.set(0, 0, 0);
     scene.add(character);
   });
